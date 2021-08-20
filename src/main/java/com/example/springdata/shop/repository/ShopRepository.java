6cc2bc77979c8d67337d50b0caddfa3ddd238337
pjy1368 <basejin0810@naver.com>
2021-08-20 13:56:03 +0900

    refactor: Spring Data Rest를 사용하여 보일러 플레이트 코드 제거

--- src/main/java/com/example/springdata/shop/repository/ShopRepository.java
+++ src/main/java/com/example/springdata/shop/repository/ShopRepository.java
@@ -3,10 +3,16 @@ package com.example.springdata.shop.repository;
 import com.example.springdata.shop.domain.Shop;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RepositoryRestResource
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
-    List<Shop> findAllByName(String name);
+    @RestResource(path = "name")
+    List<Shop> findAllByName(@Param("name") String name);
 
-    List<Shop> findAllByBranchName(String branchName);
+    @RestResource(path = "branchName")
+    List<Shop> findAllByBranchName(@Param("branchName") String branchName);
 }
